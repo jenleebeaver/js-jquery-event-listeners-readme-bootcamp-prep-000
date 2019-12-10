f875896f5450c6f23b285be3507a0a8a09ee395f
Jennifer Beaver <jen.lee.beaver@gmail.com>
2019-12-10 13:15:26 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,13 @@ function frameIt() {
   });
 };
 
-
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('G was pressed');
+  }
+});
+}
 
 $(document).ready(function() {
 // call functions here
